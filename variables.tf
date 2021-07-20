af43665f8db0d310f2be336c1d0b3730dd375167
Kathir <62635977+kathirMS@users.noreply.github.com>
2021-07-20 18:13:39 +0530

    updated

--- variables.tf
+++ variables.tf
@@ -1,58 +1,58 @@
 #<----profile of the provider----->
  
    variable "provider_profile"{
-   type="string"
-   default="aws-devops-test"
+   type=string
+   
 }
 
 #<----region of the provider----->
 
 variable "provider_region"{
-   type="string"
-   default="us-east-1"
+   type=string
+   
 }
 
 #<----Email of Topic----->
 
 variable "email"{
-   type="string"
-   default="w7q9c4x9d0l9d8a6@batonsystems.slack.com"
+   type=string
+   
 }
 
 #<-----Name Of SNS------>
 
 variable "name" {
    
-   type="string"    
-   default = "All_Ec2_Instance_State_Change_Alert"
+   type=string    
+   
 }
 
 #<-----tag_key_of_instance------->
 
 variable "tag_key" {
-   type="string"
-   default="env1"
+   type=string
+   
 }
 
 #<-----tag_value_of_instance------->
 
 variable "tag_value" {
-   type="string"
-   default="test1"
+   type=string
+   
 }
 
 #<-----tag_value_of_customer------->
 
 variable "customer" {
-   type="string"
-   default="citi"
+   type=string
+   
 }
 
 #<-----tag_value_of_cost_center------->
 
 variable "cost_center" {
-   type="string"
-   default="test1"
+   type=string
+   
 }
 
 
