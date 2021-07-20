63f6168406e9a351e3c3c29501883228695c06ed
Kathir <62635977+kathirMS@users.noreply.github.com>
2021-07-20 09:14:51 +0530

    update vars

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 #<----profile of the provider----->
  
    variable "rovider_profile"{
-   type="string"
+   type=string
    default="aws-devops-test"
 }
 
 #<----region of the provider----->
 
 variable "provider_region"{
-   type="string"
+   type=string
    default="us-east-1"
 }
 
 #<----Email of Topic----->
 
 variable "email"{
-   type="string"
+   type=string
    default="w7q9c4x9d0l9d8a6@batonsystems.slack.com"
 }
 
@@ -23,35 +23,35 @@ variable "email"{
 
 variable "name" {
    
-   type="string"    
+   type=string    
    default = "All_Ec2_Instance_State_Change_Alert"
 }
 
 #<-----tag_key_of_instance------->
 
 variable "tag_key" {
-   type="string"
+   type=string
    default="env1"
 }
 
 #<-----tag_value_of_instance------->
 
 variable "tag_value" {
-   type="string"
+   type=string
    default="test1"
 }
 
 #<-----tag_value_of_customer------->
 
 variable "customer" {
-   type="string"
+   type=string
    default="citi"
 }
 
 #<-----tag_value_of_cost_center------->
 
 variable "cost_center" {
-   type="string"
+   type=string
    default="test1"
 }
 
